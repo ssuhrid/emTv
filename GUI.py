d449dae49691d97ca0107a4d9eae121d6aee4aab
ssuhrid <suhrid.srivastava@gmail.com>
2017-06-15 02:57:57 +0530

    lan working

--- GUI.py
+++ GUI.py
@@ -1,75 +1,112 @@
 #!/usr/bin/python
 
 from Tkinter import *
+import tempfile
+from smb.SMBConnection import SMBConnection
 
-class Gui:
+def readText():
+    reading = [];
+    try:
+        fo = open("data/messages.txt", "r+");
+        for i in range(0, 5):
+            reading.append(fo.readline());
+        # Close opend file
+        fo.close();
+    except:
+        pass
 
-    def readText(self):
-        mess = [];
-        try:
-            fo = open("data/messages.txt", "r+");
+    return reading
 
+def checkRetrievedFile(file_obj):
+    copy_file_object = open('data/messages.txt', 'w');
+    file_obj.seek(0);
+    for i in range(0, 5):
+        temp = file_obj.readline();
+        copy_file_object.write(temp);
+    return True;
+
+#### to make ui full screen
+def toggle_geom(self,event):
+    geom=self.master.winfo_geometry()
+    print(geom,self._geom)
+    self.master.geometry(self._geom)
+    self._geom=geom
+
+def guiInit(master):
+    row=0
+
+    # #### Set fullscreen
+    # self.master=master
+    # pad=3
+    # self._geom='200x200+0+0'
+    # master.geometry("{0}x{1}+0+0".format(
+    #     master.winfo_screenwidth()-pad, master.winfo_screenheight()-pad))
+    # master.bind('<Escape>',self.toggle_geom)
+    # # master.overrideredirect(1)
+    #
+    # ###############################################################
+    global mess;
+
+    _mess=readText();
+
+    f0 = Frame(master)
+
+    for i in range(0,5):
+        temp = Label(f0,text=_mess[i],bg="#0000ff",height=5,width=194,fg="#ffffff") #Status Green
+        _labels.append(temp);
+        _labels[i].grid(row=i,column=0)
+
+    f0.grid(row=0)
+    loop();
+
+def loop():
+    global _mess;
+    temp = getLanFile();
+    if temp!=_mess and temp!=[] :
+        _mess = temp;
+        for i in range(0, 5):
+            print _mess[i];
+            _labels[i].config(text=temp[i]);
+            _root.update_idletasks()
+            _root.update()
+    _root.after(10000,loop);
+
+def lanConnectionInit():
+    global _conn;
+    _conn = SMBConnection('username', '', ' ', ' ', '',  # use_ntlm_v2=True,
+                         # sign_options=SMBConnection.SIGN_WHEN_SUPPORTED,
+                         is_direct_tcp=True)
+    connected = _conn.connect('192.168.1.10', 445)
+    Response = _conn.listShares(timeout=30)
+
+    for i in range(len(Response)):
+        print("Share[", i, "] =", Response[i].name)
+
+def getLanFile():
+
+    global _conn;
+    file_obj = tempfile.NamedTemporaryFile()
+    reading = [];
+    try:
+        file_attributes, filesize = _conn.retrieveFile('F_Rai\'s', '/lan-tv/data/messages.txt', file_obj);
+        print filesize;
+        if checkRetrievedFile(file_obj):
+            reading = readText();
             for i in range(0, 5):
-                mess.append(fo.readline());
-            # Close opend file
-            fo.close();
-        except:
-            pass
+                print reading[i];
+        file_obj.close();
+    except:
+        file_obj.close();
 
-        return mess
+    if reading == []:
+        return _mess;
+    else:
+        return reading;
 
-    #### to make ui full screen
-    def toggle_geom(self,event):
-        geom=self.master.winfo_geometry()
-        print(geom,self._geom)
-        self.master.geometry(self._geom)
-        self._geom=geom
+_mess = [];
+_labels = [];
+lanConnectionInit()
 
-    def __init__(self,master):
-        row=0
-
-        #### Set fullscreen
-        self.master=master
-        pad=3
-        self._geom='200x200+0+0'
-        master.geometry("{0}x{1}+0+0".format(
-            master.winfo_screenwidth()-pad, master.winfo_screenheight()-pad))
-        master.bind('<Escape>',self.toggle_geom)
-        # master.overrideredirect(1)
-
-        ###############################################################
-
-        self.mess=self.readText();
-
-        self.labels = [];
-        f0 = Frame(master)
-
-        for i in range(0,5):
-            temp = Label(f0,text=self.mess[i],bg="#0000ff",height=5,width=194,fg="#ffffff") #Status Green
-            self.labels.append(temp);
-            self.labels[i].grid(row=i,column=0)
-
-        f0.grid(row=0)
-        self.loop();
-
-    def loop(self):
-        mess = [];
-        # while True:
-        print 'sex'
-        temp = self.readText();
-        if temp!=self.mess and temp!=[] :
-            mess = temp;
-            for i in range(0, 5):
-                print mess[i];
-                self.labels[i].config(text=mess[i]);
-                root.update_idletasks()
-                root.update()
-        root.after(5000,self.loop);
-
-root = Tk()
-##root.title('AVR Uploader v2.3')
-b=Gui(root)
-# root.after(5000,b.loop);
-root.mainloop()
-# root.update_idletasks()
-# root.update()
+_root = Tk()
+guiInit(_root)
+_root.mainloop()
---
+++ lanCheck.py
@@ -0,0 +1,32 @@
+import tempfile
+from smb.SMBConnection import SMBConnection
+
+conn = SMBConnection('username','',' ',' ','',# use_ntlm_v2=True,
+    # sign_options=SMBConnection.SIGN_WHEN_SUPPORTED,
+    is_direct_tcp=True)
+
+connected = conn.connect('192.168.1.10',445)
+Response = conn.listShares(timeout=30)
+
+for i in range(len(Response)):
+    print("Share[",i,"] =", Response[i].name)
+
+# file_obj = tempfile.NamedTemporaryFile()
+# file_attributes, filesize = conn.retrieveFile('F_Rai\'s','/lan-tv/data/messages.txt',file_obj);
+# print filesize;
+#
+# mess = [];
+# copy_file_object = open('data/copied.txt', 'w');
+# try:
+#     file_obj.seek(0);
+#     for i in range(0, 5):
+#         temp = file_obj.readline();
+#         copy_file_object.write(temp);
+#         mess.append(temp);
+#     # Close opend file
+# except:
+#     pass
+#
+# for i in range(0,5):
+#     print mess[i];
+
